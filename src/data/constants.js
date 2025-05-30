4a9756169e17ee49376e798ff3e32afd07e3c26f
Paresh099 <pareshsolanki1646@gmail.com>
2025-05-30 08:22:21 +0530

    edu chnage

--- src/data/constants.js
+++ src/data/constants.js
@@ -181,31 +181,23 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-    date: "Oct 2021 - Sep 2025",
-    grade: "8.71 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    img: "https://th.bing.com/th/id/OIP.OgIln3EAwpZ5UADF-G7g-wHaHZ?rs=1&pid=ImgDetMain",
+    school: "Charusat University, Anand",
+    date: "June 2023 - May 2025",
+    grade: "Distinction",
+    desc: "I have completed a Master of Computer Applications (MCA).",
+    degree: " Master of Computer Applications (MCA)",
   },
   {
     id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2019 - Apr 2025",
-    grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-    degree: "ISC(XII), Science with Computer",
+    img: "https://th.bing.com/th/id/OIP.OgIln3EAwpZ5UADF-G7g-wHaHZ?rs=1&pid=ImgDetMain",
+    school: "Charusat University, Anand",
+    date: "June 2020 - May 2023",
+    grade: "Distinction",
+    desc: "I have completed a Bachelor of Science in Information Technology (B.Sc. IT)",
+    degree: "Bachelor of Science in Information Technology (B.Sc IT)",
   },
-  {
-    id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
-  },
+
 ];
 
 export const projects = [
